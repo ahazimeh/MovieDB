667fb24407eea8f2f3377b363a673b50c16ef60a
ahazimeh <hazimeh95@gmail.com>
2020-10-21 12:35:59 +0300

    Step 2

---
+++ express.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.listen(port, function () {
+    console.log("Server is running on "+ port +" port");
+});
+app.get(URL,function(req,res){
+    res.send('ok');
+})
