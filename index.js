f3a3b8f75d1843c05854c79a3d9eda709d2d8dff
ahazimeh <hazimeh95@gmail.com>
2020-10-21 15:37:21 +0300

    Step 5

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ app.listen(port, function () {
     console.log("Server is running on "+ port +" port");
 });
 
-
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 app.get('/test',function(req,res){
     res.status(200).send('ok');
     
@@ -33,6 +38,8 @@ app.get("/hello/:id",function(req,res){
 });
 
 app.get('/search',function(req,res){
+    if(typeof(req.query.s)=="undefined")
+    res.send("aa");
     if(req.query.s==""){
         res.status(500).send("error:true, you have to provide a search");
     }
@@ -40,4 +47,20 @@ app.get('/search',function(req,res){
         res.status(200).send("ok, data:"+req.query.s);
         
     }
+});
+
+app.get('/movies/create',function(req,res){
+    
+});
+
+app.get('/movies/read',function(req,res){
+    res.status(200).send(movies);
+});
+
+app.get('//movies/update',function(req,res){
+    
+});
+
+app.get('/movies/delete',function(req,res){
+    
 });
